71d2f3f04dc8a260a4f283e1ec811336a2dc522b
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-06-04 01:10:21 +0530

    Added code to create contact

--- Address_Book.js
+++ Address_Book.js
@@ -1,1 +1,34 @@
-console.log("welcome to Address Book project")
+console.log("Welcome to JavaScript Address Book Problem solution");
+class Contact {
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.address = address;
+      this.city = city;
+      this.state = state;
+      this.zip = zip;
+      this.phoneNumber = phoneNumber;
+      this.email = email;
+    }
+  }
+  
+  const contact1 = new Contact(
+    "Sudarshan",
+    "Gaikwad",
+    "123 street",
+    "Pandharpur",
+    "Maharshtra",
+    "413304",
+    "8308622248",
+    "sudarshangaikwad@gmail.com"
+  );
+  
+  console.log("First Name:", contact1.firstName);
+  console.log("Last Name:", contact1.lastName);
+  console.log("Address:", contact1.address);
+  console.log("City:", contact1.city);
+  console.log("State:", contact1.state);
+  console.log("Zip:", contact1.zip);
+  console.log("Phone Number:", contact1.phoneNumber);
+  console.log("Email:", contact1.email);
+  
